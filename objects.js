c7e5a2b93df62edf9429967d9e9e7fa885f03069
Hilary Sbei <hilsbei@gmail.com>
2017-12-15 17:54:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,10 +6,11 @@ function createPlaylist(artistName, songTitle) {
   var playlist = createPlaylist("Childish Gambino", "Redbone");
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var playlist = {artistName, "songTitle"};
-  playlist = [artistName: "Phil Ochs, songTitle: "Here's to the State of Mississippi'];
-  return playlist;
+  var obj = {[artistName]: songTitle}
+  return obj;
 }
+
+  var playlist = [artistName: "Phil Ochs, songTitle: "Here's to the State of Mississippi'];
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
