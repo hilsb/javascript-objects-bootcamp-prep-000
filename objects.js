997c3c54a68c48536df8ebade6d38326adab4e69
Hilary Sbei <hilsbei@gmail.com>
2017-12-15 17:42:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function createPlaylist(artistName, songTitle) {
   return obj;
 }
 
-  var playlist = createPlaylist();
+  var playlist = createPlaylist("Childish Gambino", "Redbone");
 
 function updatePlaylist(playlist, artistName, songTitle) {
   var playlist = {artistName, "songTitle"};
