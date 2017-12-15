58b7ddc8d9df88e1e72e349e58e3761a01d189aa
Hilary Sbei <hilsbei@gmail.com>
2017-12-15 18:04:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,14 +3,10 @@ function createPlaylist(artistName, songTitle) {
   return obj;
 }
 
-  var playlist = createPlaylist(artistName, songTitle);
-
 function updatePlaylist(playlist, artistName, songTitle) {
   var obj = {[artistName]: songTitle}
   return obj;
 }
-
-  playlist.updatePlaylist = (artistName: "Phil Ochs, songTitle: "Here\'s to the State of Mississippi');
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
