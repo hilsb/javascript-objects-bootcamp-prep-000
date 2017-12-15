b7b7034dcc7425a9dfb8b70c32319a2084a46078
Hilary Sbei <hilsbei@gmail.com>
2017-12-15 18:09:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,6 +11,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 var playlist = {key: 'value'};
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
